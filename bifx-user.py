75a1d42ec78a2a87da80346c80ca7b146cd49e2b
John Dow <jmd@nelefa.org>
2020-12-11 13:14:31 +0000

    Updated doc

--- bifx-user.py
+++ bifx-user.py
@@ -2,7 +2,7 @@
 
 # Simple script to read a CSV of one user per line
 # In the format:
-# Username, user id, group id, sudo access
+# Username, user id, group id, sudo access, home directory with trailing slash
 # Where sudo access is either Y or N
 #
 # Version 1
